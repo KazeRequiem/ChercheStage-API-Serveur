b675368b9706b4b74af717e99b8a203c88cb170b
Baptiste Noisette <90211737+KazeRequiem@users.noreply.github.com>
2025-04-01 14:09:39 +0200

    Update test.php

--- test.php
+++ test.php
@@ -18,10 +18,6 @@ if (!extension_loaded('pdo_mysql')) {
 
 try {
 
-    //User_model::createUser('Yanis','Barral','yanis.barral@domain.fr','test123','0745621458','2006-09-07',0,1);
-    //User_model::createUser('Baptiste','Noisette','baptiste.noisette@domain.fr','mdp123','0612345678','2005-12-12',2,2);
-    //User_model::createUser('Sab','Carp','sab.carp@domain.fr','test123','0687654321','2003-03-25',1,2);
-    //print_r(User_model::getAllUsers());
     //print_r(User_model::getUserById(3));
 
     //print_r(Promotion_model::getPromotionById(1));
